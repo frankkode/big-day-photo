346918c91f3a46e95b2cda43190da4f1c8b7b896
masacode <masasimpafra@gmail.com>
2022-03-14 21:36:26 +0100

    add device size

--- src/components/ClCamera/index.js
+++ src/components/ClCamera/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCameraRotate,faTimesSquare } from '@fortawesome/free-solid-svg-icons'
+import { faCameraRotate, faTimesSquare } from '@fortawesome/free-solid-svg-icons'
 
 class camera extends Component {
      constructor() {
@@ -19,6 +19,24 @@ class camera extends Component {
 
      initializeMedia = async () => {
           this.setState({ imageDataURL: null });
+          if (!("mediaDevices" in navigator)) {
+               navigator.mediaDevices = {};
+          }
+
+          if (!("getUserMedia" in navigator.mediaDevices)) {
+               navigator.mediaDevices.getUserMedia = function (constraints) {
+                    var getUserMedia =
+                         navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                    if (!getUserMedia) {
+                         return Promise.reject(new Error("getUserMedia Not Implemented"));
+                    }
+
+                    return new Promise((resolve, reject) => {
+                         getUserMedia.call(navigator, constraints, resolve, reject);
+                    });
+               };
+          }
 
           //Get the details of video inputs of the device
           const videoInputs = await this.getListOfVideoInputs();
@@ -26,7 +44,7 @@ class camera extends Component {
           //The device has a camera
           if (videoInputs.length) {
                navigator.mediaDevices
-                    .getUserMedia({ video: {width:400} })
+                    .getUserMedia({ video: { width: 400 } })
                     .then((stream) => {
                          this.player.srcObject = stream;
                     })
